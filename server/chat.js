d12bc132a1ea654e1068fe3a85ee0e37026042da
vadopolski <vop444@gmail.com>
2018-09-22 21:51:19 +0300

    test

--- server/chat.js
+++ server/chat.js
@@ -1,38 +1,29 @@
 clients = {};
 messages = {};
 
-var http = require('http');
-var url = require('url');
-
-
-http.createServer(function (req, res) {
-    res.end('is_response');
-}).listen(8080);
-
-var ws = require('ws').Server,
-    wss = new ws({port: 8081});
+var ws = require('ws').Server;
+var wss = new ws({port: 8001});
 
 wss.on('connection', function (ws) {
-    console.log('connected user');
     ws.on('message', function (m) {
-         console.log('->>' + m);
-        // ws.send('PONG');
+        m = JSON.parse(m);
         switch (m.op_type){
             case 'init':
-                connectUser(m.user_id);
+                connectUser(m.user_id, ws);
                 break;
             case 'message':
-                messages.push(m.message);
                 for (var i in clients){
                     if(i == m.user_id)
                         continue;
-                    ws.send(m.message);
+                    if(clients[i].readyState != 1)
+                        continue;
+                    clients[i].send(m.message);
                 }
                 break;
         }
     });
 });
 
-function connectUser(user_id) {
-    clients[user_id] = {}
+function connectUser(user_id, sock) {
+    clients[user_id] = sock;
 }
---
+++ server/test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Home on 22.09.2018.
+ */
+
+obj = {};
+
+const i = new Number(777);
+
+console.log(obj);
+
+var girl = 77;
+
+obj[girl] = 18;
+
+console.log(obj);
