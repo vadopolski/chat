7150da0745496bfc035f4729e8306a01c0e00bff
vadopolski <vop444@gmail.com>
2018-09-22 22:06:50 +0300

    Refactoring

--- server/chat.js
+++ server/chat.js
@@ -7,15 +7,15 @@ var wss = new ws({port: 8001});
 wss.on('connection', function (ws) {
     ws.on('message', function (m) {
         m = JSON.parse(m);
-        switch (m.op_type){
+        switch (m.op_type) {
             case 'init':
                 connectUser(m.user_id, ws);
                 break;
             case 'message':
-                for (var i in clients){
-                    if(i == m.user_id)
+                for (var i in clients) {
+                    if (i == m.user_id)
                         continue;
-                    if(clients[i].readyState != 1)
+                    if (clients[i].readyState != 1)
                         continue;
                     clients[i].send(m.message);
                 }
